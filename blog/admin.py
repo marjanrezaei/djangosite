a80169c631dcddac1cd4dbdd61c1fec7cd5135f7
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-18 13:30:50 +0330

    mange admin for post
    add some ability for post in amin

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+# @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display =('title', 'status', 'published_date', 'content_view')
+    empty_value_dispaly = '-empty-'
+    # fields = ('title',)
+    # exclude = ('title',)
+    list_filter = ('status',)
+    ordering = ('-published_date',)
+    search_fields = ('title', 'content')
+
+
+
+admin.site.register(Post, PostAdmin)
+
