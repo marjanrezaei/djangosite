4b471df98fd2c4d5688dbb24e9fba7b5679d61d7
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-25 18:45:23 +0330

    create template tag for category

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template 
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -21,3 +21,15 @@ def snippet(value,arg=10):
 def latestposts(arg=2):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts': posts}
+
+@register.inclusion_tag('blog//blog-post-category.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories': cat_dict}
+
+
+
