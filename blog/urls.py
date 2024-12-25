bbe2653d36265594e905d29a6fccf98792da4315
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-25 22:04:25 +0330

    show posts based on author

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', index, name = 'index'),
     path('<int:pid>', single, name = 'single'),
-     path('category/<str:cat_name>', index, name = 'category'),
+    path('category/<str:cat_name>', index, name = 'category'),
+    path('author/<str:author_username>', index, name = 'author'),
     path('test', test, name='test'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
-def index(request, cat_name=None):
+def index(request, **kwargs):
     posts = Post.objects.filter(status=1)
-    if cat_name:
-          posts = posts.filter(category__name=cat_name)
+    if kwargs.get('cat_name') != None:
+          posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+          posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts' : posts}
     return render(request, 'blog\\blog-home.html', context) 
 
@@ -13,13 +15,6 @@ def single(request, pid):
     post = get_object_or_404(Post, pk=pid, status=1)
     context = {'post' : post,}
     return render(request, 'blog\\blog-single.html', context)
-
-
-def blog_category(request, cat_name):
-    posts = Post.objects.filter(status=1)
-    posts = posts.filter(category__name=cat_name)
-    context = {'posts' : posts}
-    return render(request, 'blog\\blog-home.html', context) 
 
 def test(request):
     return render(request, 'test.html')
