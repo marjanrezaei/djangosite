7c7064ea40a71f6f0217149b80b9e3bb316529b9
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-25 17:00:02 +0330

    make popular posts with template tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -17,7 +17,7 @@ def test_show():
 def snippet(value,arg=10):
     return value[:arg] + '...'
 
-@register.inclusion_tag('popularposts.html')
-def popularposts():
-    posts = Post.objects.filter(status=1).order_by('published_date')[:2]
+@register.inclusion_tag('blog//blog-popular-post.html')
+def latestposts(arg=2):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts': posts}
