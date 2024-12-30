ececaba3420f37e9c7e8608ffe08868b0073f631
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-30 13:49:58 +0330

    create Sitemap for blog First way
    first way with getabsoluteurl

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -25,6 +26,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:single', kwargs={'pid': self.id})
+    
+    
 
 
 
