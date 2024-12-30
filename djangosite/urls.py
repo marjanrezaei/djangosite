64b25f666fa520a27f5322da319f99dae3fce51b
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-30 16:20:29 +0330

    set robots.txt
    set configuration for robots

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -32,8 +32,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('blog/', include('blog.urls')),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap",
-)
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap",),
+    path('robots.txt', include('robots.urls')),
+    
 
 ]
 
