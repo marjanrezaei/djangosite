4df9fe3431ff56239c0c710772b5739f862b94cc
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-30 19:50:19 +0330

    django debugger
    tools for work with pages and find errors

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from website.sitemaps import StaticViewSitemap
 from blog.sitemaps import BlogSitemap
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 sitemaps = {
     "static": StaticViewSitemap,
@@ -36,7 +37,7 @@ urlpatterns = [
     path('robots.txt', include('robots.urls')),
     
 
-]
+] + debug_toolbar_urls()
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
