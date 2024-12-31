b3c994db54abbbc9194240a678628b7a8e405771
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-31 15:48:26 +0330

    added captcha to contact.html

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'taggit',
     'django_summernote',
+    'captcha',
 ]
 
 # site framework
--- djangosite/urls.py
+++ djangosite/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap",),
     path('robots.txt', include('robots.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('captcha/', include('captcha.urls')),
 
 ] + debug_toolbar_urls()
 
