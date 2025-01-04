20552fcd1dd413846cb7489e094c7a24886d04c9
marjanrezaei <rezaei.marjann@gmail.com>
2025-01-04 19:01:30 +0330

    User authenticated by template

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f'You are logged in as {request.user.username}'
-    else:
-        msg = 'You are  not logged in'
-    return render(request, 'accounts\login.html', {'msg': msg}) 
+    return render(request, 'accounts\login.html') 
 
 # def logout_view(request):
     # return 
 
 def signup_view(request):
     return render(request, 'accounts\signup.html') 
+
 
 
