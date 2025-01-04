e3e437e5510e1e0779b83bdcaf942ce6259ceaa9
marjanrezaei <rezaei.marjann@gmail.com>
2025-01-04 15:28:53 +0330

    Rss feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from blog.views import *
+from blog.feeds import LatestEntriesFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -9,5 +10,6 @@ urlpatterns = [
     path('tag/<str:tag_name>', index, name = 'tag'),
     path('author/<str:author_username>', index, name = 'author'),
     path('search/', search, name = 'search'),
-  
+    path("rss/feed/", LatestEntriesFeed()),
+    
 ]
