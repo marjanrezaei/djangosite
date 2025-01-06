eec8e7a186bdbb98ef9e1f50a466d8236e9ca6e6
marjanrezaei <rezaei.marjann@gmail.com>
2025-01-06 20:23:06 +0330

    set default settings for deploy

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -16,16 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aeesaeey&jtvkj)&2^2ogo3wqdeh8@e$6g98%83#_z7n1#zn36'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -52,8 +43,7 @@ INSTALLED_APPS = [
     
 ]
 
-# site framework
-SITE_ID = 2
+
 
 # robots
 ROBOTS_USE_HOST = True
@@ -96,15 +86,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djangosite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -141,15 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'statics',
-]
+
 
 
 # Default primary key field type
@@ -160,3 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
